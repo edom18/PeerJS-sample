4378158cf6c1ab3750e7d598f6b9c2e3b6032a2d
edom18 <edo.m18@gmail.com>
2014-06-07 14:06:51 +0900

    added.

---
+++ js/leap-test.js
@@ -0,0 +1,49 @@
+(function (win, doc) {
+
+    'use strict';
+
+    var riggedHandPlugin;
+    Leap.loop({
+        hand: function(hand){
+            var label = hand.data('label');
+
+            if (!label){
+
+                label = document.createElement('label');
+                document.body.appendChild(label);
+
+                /**
+                 * Here we set the label to show the hand type
+                 */
+                label.innerHTML = hand.type + " hand";
+                hand.data('label', label);
+            }
+
+
+            var handMesh = hand.data('riggedHand.mesh');
+
+            var screenPosition = handMesh.screenPosition(
+                hand.palmPosition,
+                riggedHandPlugin.camera
+            );
+
+            label.style.left = screenPosition.x + 'px';
+            label.style.bottom = screenPosition.y + 'px';
+        }
+    })
+    .use('riggedHand')
+        .use('handEntry')
+        .on('handFound', function(hand) {
+            console.log('hoge');
+        })
+        .on('handLost', function(hand){
+            var label = hand.data('label');
+            if (label){
+                document.body.removeChild(label);
+                hand.data({label: undefined});
+            }
+        });
+
+    riggedHandPlugin = Leap.loopController.plugins.riggedHand;
+
+}(window, document));
